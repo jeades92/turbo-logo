320b5ce9f49e4aabc2fc7c0f4754c7bbab8c80cd
Jimmys Mac <eadesj25@gmail.com>
2023-04-16 11:01:17 -0400

    creates svg file and answers prompts, has error in filepath tho

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
+const { Circle, Triangle } = require('./library/shapes')
 
 function showQuestions() {
     inquirer.prompt([
@@ -9,7 +10,7 @@ function showQuestions() {
             message: "Choose a shape for your logo:",
             choices: ['circle', 'triangle', 'square']
         },
-    
+
         {
             type: 'input',
             name: 'shapeColor',
@@ -28,11 +29,11 @@ function showQuestions() {
           message: "What color will the text be?"
         }    ])
     .then(answers => {
-        fs.writeFile('package.json', JSON.stringify(answers, null, 2), (err) => {
+        fs.writeFile('logo.svg', JSON.stringify(answers, null, 2), (err) => {
             if (err) {
                 console.error(err);
             } else {
-                console.log('Answers saved to package.json');
+                console.log('Generated logo.svg');
             }
         });
     })
